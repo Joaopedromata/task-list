d4400d6f1da4dea94a6291e28b36b5db09c2706d
joaopedromata <joaopmata182@gmail.com>
2024-03-14 12:00:02 -0300

    add header to mobile version

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import "./styles.css";
-import { useNavigate } from "react-router-dom";
-import logout from "../../assets/log-out.svg";
+import { Link, useNavigate } from "react-router-dom";
 import arrowLeft from "../../assets/arrow-left.svg";
+import menu from "../../assets/menu.svg";
+import { useState } from "react";
+import x from "../../assets/x.svg";
 
 function Header(props) {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
   function handleLogout() {
     localStorage.removeItem("token");
@@ -13,17 +16,39 @@ function Header(props) {
 
   return (
     <header className="header">
+      {showMenu && (
+        <div className="header-menu">
+          <Link className="header-menu-list-item" to="/boards">
+            Quadros
+          </Link>
+          <Link className="header-menu-list-item">Financeiro</Link>
+          <div className="header-menu-list-item" onClick={handleLogout}>
+            Sair
+          </div>
+          <div
+            className="header-menu-list-item"
+            onClick={() => setShowMenu(false)}
+          >
+            <img src={x} />
+          </div>
+        </div>
+      )}
+      <img
+        src={menu}
+        onClick={() => setShowMenu(true)}
+        className="header-icon menu"
+      />
       {props.backTo ? (
         <img
           src={arrowLeft}
           onClick={() => navigate(props.backTo)}
-          className="header-icon"
+          className="header-icon back"
         />
       ) : (
-        <div />
+        <div className="fake-div" />
       )}
       <h1 className="header-title">{props.title}</h1>
-      <img src={logout} onClick={handleLogout} className="header-icon" />
+      <div />
     </header>
   );
 }
--- frontend/src/components/SideBar/index.jsx
+++ frontend/src/components/SideBar/index.jsx
@@ -3,13 +3,20 @@ import board from "../../assets/board.svg";
 import money from "../../assets/money.svg";
 import logo from "../../assets/logo.svg";
 import { Link, useNavigate } from "react-router-dom";
+import logout from "../../assets/log-out.svg";
 
 const SideBar = () => {
   const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    navigate("/");
+  }
+
   return (
     <div className="side-bar">
       <img src={logo} className="logo" onClick={() => navigate("/boards")} />
-      <div className="side-bar-list">
+      <div className="side-bar-list-top">
         <Link className="side-bar-list-item" to="/boards">
           <img src={board} />
           Quadros
@@ -18,6 +25,13 @@ const SideBar = () => {
           <img src={money} />
           Financeiro
         </Link>
+      </div>
+      <hr className="side-bar-separator" />
+      <div className="side-bar-list-bottom">
+        <div className="side-bar-list-item" onClick={handleLogout}>
+          <img src={logout} />
+          Sair
+        </div>
       </div>
     </div>
   );
